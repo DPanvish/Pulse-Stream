4fdab7e143724849aa4c6bde65e528f3b9afc456
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-12-25 20:57:41 +0530

    Api ti add token

---
+++ frontend/src/api/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:5000/api",
+})
+
+// Interceptor: Automatically add the Token to headers if it exists
+api.interceptors.request.use((config) => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if(user && user.token){
+        config.headers.Authorization = `Bearer ${user.token}`;
+    }
+
+    return config;
+},
+    (error) => {
+        Promise.reject(error);
+    }
+);
+
+export default api;
